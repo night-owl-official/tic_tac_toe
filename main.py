6f53d24c72738841d02883bf7babc8fcca7c5141
Daniel Grieco <daniel_grieco@programmer.net>
2021-07-13 01:29:19 +0200

    Create function that updates a board value at the given position with the given marker.

--- main.py
+++ main.py
@@ -18,5 +18,19 @@ def display_board(board_values: list):
     print(f" {board_values[0]} | {board_values[1]} | {board_values[2]} ")
 
 
+def update_board(new_value: str, at_position: int, board_values: list):
+    """
+    Updates the board values by setting the given marker at the given position in the list.
+
+        Parameters:
+            new_value (str): A value to set on the board (O, X, ' ').
+            at_position (int): A position representing one of the available positions on the board (numpad).
+            board_values (list): A list for the values currently set on the board.
+    """
+
+    # Update the given board value
+    board_values[at_position - 1] = new_value
+
+
 # The tic tac toe board values
 ttt_board_values = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
